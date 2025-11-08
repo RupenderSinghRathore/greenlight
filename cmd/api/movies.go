9ba3bbfc40497215527ff9c7dfb8af2029e51a15
kami-sama <kamisamakirishima@gmail.com>
2025-11-08 11:45:50 +0530

    custom unmarshalJson runtime

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,10 +10,10 @@ import (
 func (app application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintln(w, "create a new movie")
 	var input struct {
-		Title   string   `json:"title"`
-		Runtime int32    `json:"runtime"`
-		Year    int32    `json:"year"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Runtime data.Runtime `json:"runtime"`
+		Year    int32        `json:"year"`
+		Genres  []string     `json:"genres"`
 	}
 	err := readJSON(w, r, &input)
 	if err != nil {
