2b3bc1332067991ef6b5c0507e086715dbf64577
kami-sama <kamisamakirishima@gmail.com>
2025-11-09 18:07:59 +0530

    validation setup

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -5,14 +5,15 @@ import (
 	"net/http"
 
 	"github.com/RupenderSinghRathore/greenlight/internal/data"
+	"github.com/RupenderSinghRathore/greenlight/internal/validator"
 )
 
 func (app application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintln(w, "create a new movie")
 	var input struct {
 		Title   string       `json:"title"`
-		Runtime data.Runtime `json:"runtime"`
 		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
 		Genres  []string     `json:"genres"`
 	}
 	err := readJSON(w, r, &input)
@@ -20,8 +21,23 @@ func (app application) createMovieHandler(w http.ResponseWriter, r *http.Request
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	v := validator.New()
+
+	movie := data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	if data.ValidateMovie(v, &movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
+
 func (app application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := readIdParams(r)
 	if err != nil {
